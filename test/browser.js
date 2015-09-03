b6092c7e1d0ec6e2ce48b35982d2bfbbeb555246
Eran Hammer <eran@hammer.io>
2015-09-03 08:43:05 -0700

    socket state cleanup

--- test/browser.js
+++ test/browser.js
@@ -32,7 +32,50 @@ describe('Browser', function () {
                 client.request('GET', '/', function (err, payload, statusCode, headers) {
 
                     expect(err).to.exist();
+                    expect(err.message).to.equal('Disconnected');
                     done();
+                });
+            });
+        });
+
+        describe('_onClose()', function () {
+
+            it('errors on pending request when closed', function (done) {
+
+                var server = new Hapi.Server();
+                server.connection();
+                server.register({ register: Nes, options: {} }, function (err) {
+
+                    expect(err).to.not.exist();
+
+                    server.route({
+                        method: 'GET',
+                        path: '/',
+                        handler: function (request, reply) {
+
+                            setTimeout(function () {
+
+                                return reply('hello');
+                            }, 100);
+                        }
+                    });
+
+                    server.start(function (err) {
+
+                        var client = new Nes.Client();
+                        client.connect('http://localhost:' + server.info.port, function () {
+
+                            client.request('GET', '/', function (err, payload, statusCode, headers) {
+
+                                expect(err).to.exist();
+                                expect(err.message).to.equal('Disconnected');
+
+                                server.stop(done);
+                            });
+
+                            client.disconnect();
+                        });
+                    });
                 });
             });
         });
