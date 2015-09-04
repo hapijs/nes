b9a34471182af0153989cdcd2e39cc1068c7209c
Eran Hammer <eran@hammer.io>
2015-09-03 18:31:15 -0700

    broadcast event

--- test/listener.js
+++ test/listener.js
@@ -34,55 +34,18 @@ describe('Listener', function () {
 
                 server.start(function (err) {
 
-                    var client = new Ws('http://localhost:' + server.info.port);
+                    var client = new Nes.Client('http://localhost:' + server.info.port);
+                    client.onBroadcast = function (message) {
 
-                    client.on('message', function (data, flags) {
-
-                        var message = JSON.parse(data);
-                        expect(message.payload).to.equal('hello');
-                        expect(message.headers).to.deep.equal({});
-                        expect(message.statusCode).to.equal(200);
-                        expect(message.type).to.equal('broadcast');
-
-                        client.close();
+                        expect(message).to.equal('hello');
+                        client.disconnect();
                         server.stop(done);
-                    });
+                    };
 
-                    client.on('open', function () {
+                    client.connect(function (err) {
 
+                        expect(err).to.not.exist();
                         server.broadcast('hello');
-                    });
-                });
-            });
-        });
-
-        it('sends message to all clients with headers', function (done) {
-
-            var server = new Hapi.Server();
-            server.connection();
-            server.register({ register: Nes, options: {} }, function (err) {
-
-                expect(err).to.not.exist();
-
-                server.start(function (err) {
-
-                    var client = new Ws('http://localhost:' + server.info.port);
-
-                    client.on('message', function (data, flags) {
-
-                        var message = JSON.parse(data);
-                        expect(message.payload).to.equal('hello');
-                        expect(message.headers).to.deep.equal({ test: 1 });
-                        expect(message.statusCode).to.equal(200);
-                        expect(message.type).to.equal('broadcast');
-
-                        client.close();
-                        server.stop(done);
-                    });
-
-                    client.on('open', function () {
-
-                        server.broadcast('hello', { test: 1 });
                     });
                 });
             });
@@ -94,28 +57,22 @@ describe('Listener', function () {
             server.connection();
             server.register({ register: Nes, options: {} }, function (err) {
 
-                server.connection();
-
                 expect(err).to.not.exist();
 
                 server.start(function (err) {
 
-                    var client = new Ws('http://localhost:' + server.info.port);
+                    var client = new Nes.Client('http://localhost:' + server.info.port);
+                    client.onBroadcast = function (message) {
 
-                    client.on('message', function (data, flags) {
-
-                        var message = JSON.parse(data);
-                        expect(message.payload).to.equal('hello');
-                        expect(message.headers).to.deep.equal({});
-                        expect(message.statusCode).to.equal(200);
-                        expect(message.type).to.equal('broadcast');
-
-                        client.close();
+                        expect(message).to.equal('hello');
+                        client.disconnect();
                         server.stop(done);
-                    });
+                    };
 
-                    client.on('open', function () {
+                    client.connect(function (err) {
 
+                        expect(err).to.not.exist();
+                        server.connection();
                         server.broadcast('hello');
                     });
                 });
