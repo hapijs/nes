6b55e1878d85cd8e7679d4977e3212c2c276eefe
Eran Hammer <eran@hammer.io>
2017-11-13 12:49:40 -0800

    Fix test. Closes #202

--- test/client.js
+++ test/client.js
@@ -325,9 +325,9 @@ describe('Client', () => {
                     const client = new Nes.Client('http://localhost:' + server.info.port);
 
                     const orig = client._connect;
-                    client._connect = (...args) => {
+                    client._connect = (options, initial, callback) => {
 
-                        orig.apply(client, args);
+                        orig.call(client, options, initial, callback);
                         client._ws.onerror = client._ws.onclose;
                     };
 
